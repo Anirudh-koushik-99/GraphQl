2ea213ed2b25c7ff5800a4c3cb41504c6d14b4fc
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-08-12 23:57:58 +0530

    Header Component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+const Header = () => {
+  return (
+    <nav className="navbar bg-light mb-4 p-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+            <strong>Project Management</strong>
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
