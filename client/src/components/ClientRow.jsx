15cbb341112e8da9014cd018759ee6c0417e83bb
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-08-15 21:11:48 +0530

    Delete client, Reflect change using InMemeoryCache

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
-import {FaTrash} from 'react-icons/fa'
+import React from "react";
+import { FaTrash } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+import { DELETE_CLIENT } from "../mutations/clientMutations";
+import { GET_CLIENTS } from "../queries/clientQueries";
 
-const ClientRow = ({client}) => {
+const ClientRow = ({ client }) => {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    /*TO REFLECT THE CHANGES MADE IN THE UI 
+    Option 1 - Refetching the query (Too many refetches will slow down application)
+
+    refetchQueries: ({query: GET_CLIENTS})*/
+
+    //Option 2 - Updating cache
+    update(cache, { data: { deleteClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: {
+          clients: clients.filter((client) => client.id !== deleteClient.id),
+        },
+      });
+    },
+  });
   return (
     <tr>
-        <td>{client.name}</td>
-        <td>{client.email}</td>
-        <td>{client.phone}</td>
-        <td>
-            <button className='btn btn-danger btn-sm'><FaTrash /></button>
-        </td>
+      <td>{client.name}</td>
+      <td>{client.email}</td>
+      <td>{client.phone}</td>
+      <td>
+        <button className="btn btn-danger btn-sm" onClick={deleteClient}>
+          <FaTrash />
+        </button>
+      </td>
     </tr>
-  )
-}
+  );
+};
 
-export default ClientRow
+export default ClientRow;
